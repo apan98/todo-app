693c417c1fb051becc78ed3eb84f5b731449a531
a.pan <a.pan@pandev.io>
2026-03-26 18:18:44 +0500

    feature/cb52ed78 - Add seed data

--- backend/server.js
+++ backend/server.js
@@ -44,4 +44,16 @@ function initial() {
         id: 3,
         title: "Done"
     });
+
+    const Task = db.tasks;
+    Task.create({ title: 'Task 1', description: 'Description 1', priority: 'low', categoryId: 1 });
+    Task.create({ title: 'Task 2', description: 'Description 2', priority: 'medium', categoryId: 1 });
+    Task.create({ title: 'Task 3', description: 'Description 3', priority: 'high', categoryId: 1 });
+    Task.create({ title: 'Task 4', description: 'Description 4', priority: 'low', categoryId: 2 });
+    Task.create({ title: 'Task 5', description: 'Description 5', priority: 'medium', categoryId: 2 });
+    Task.create({ title: 'Task 6', description: 'Description 6', priority: 'high', categoryId: 2 });
+    Task.create({ title: 'Task 7', description: 'Description 7', priority: 'low', categoryId: 3 });
+    Task.create({ title: 'Task 8', description: 'Description 8', priority: 'medium', categoryId: 3 });
+    Task.create({ title: 'Task 9', description: 'Description 9', priority: 'high', categoryId: 3 });
+    Task.create({ title: 'Task 10', description: 'Description 10', priority: 'low', categoryId: 3 });
 }
