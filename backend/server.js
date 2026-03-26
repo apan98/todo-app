94a7c402d418ed8da8b1c4a394e669ee5f7ea980
a.pan <a.pan@pandev.io>
2026-03-26 21:29:18 +0500

    feature/cb52ed78 - fix: backend routing

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const express = require("express");
 const session = require("express-session");
-const routes = require("./controllers");
+const routes = require("./routes");
 const sequelize = require("./config/config");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
