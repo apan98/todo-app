9d4fde65f35cd4b1f5cbfcc17614e629391d7b18
a.pan <a.pan@pandev.io>
2026-03-27 16:53:05 +0500

    fix: remove force:true from sequelize.sync to prevent data loss

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const taskRoutes = require('./routes/tasks');
+const { sequelize } = require('./models');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -12,6 +13,8 @@ app.use(bodyParser.json());
 
 app.use('/api/tasks', taskRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 });
