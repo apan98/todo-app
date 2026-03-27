40e8aa8bb07de51235254a0b73006cd67d9316f1
a.pan <a.pan@pandev.io>
2026-03-27 15:58:53 +0500

    fix(backend): correct routes path in index.js

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const db = require('./src/models');
 app.use(cors());
 app.use(express.json());
 
-const routes = require('./routes');
+const routes = require('./src/routes');
 app.use('/api', routes);
 
 app.listen(port, () => {
